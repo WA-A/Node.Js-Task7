3952bbf889b8491895a46238b7705d41a3be526f
wasan <wasanawwad2020@gmail.com>
2024-03-07 22:58:41 +0200

    Bearer Token

--- MiddleWare/auth.js
+++ MiddleWare/auth.js
@@ -1,10 +1,14 @@
-import  Jwt  from "jsonwebtoken";
+import  jwt  from "jsonwebtoken";
 const auth = (req,res,next)=>{
-    const {token} = req.headers;
+    const {authorization} = req.headers;
+    if(!authorization.startWith('Wasan__')){ // bearer token the before is named basic token
+        return res.json({message:"token is required"}); 
+    }
+    const token = authorization.split(process.env.BERERTOKEN)[1];
     if(!token){
         return res.json({message:"token is required"});
     }
-    const decoded =  jwt.verify(token,process.env.TOKEN);
+    const decoded =jwt.verify(token,process.env.TOKEN);
     req.userId = decoded.id;
     next();
 };
